504946a1ddd246dc6816657efc602ceea8dca671
Jonas <jonwow@pm.me>
2021-02-03 15:05:56 +0200

    demo inquire page

--- src/components/Inquire.component.js
+++ src/components/Inquire.component.js
@@ -41,24 +41,25 @@ class Inquire extends Component {
     render() {
         return (
             <div style={{height: 'inherit'}}>
-                <h1 className="p-heading1">Get in Touch</h1>
-                <Form onSubmit={this.handleSubmit.bind(this)}>
-                    <FormGroup controlId="formBasicEmail">
-                        <Label className="text-muted">Email address</Label>
-                        <Input
+                <div style={{marginTop: '9rem', fontSize: '2.2rem', textAlign: 'center'}}>
+                <h1 className="p-heading1">{this.props.match.params.lang === "LT" ? "Susisiekime" : this.props.match.params.lang === "EN" && "Get in Touch"}</h1>
+              
+                <Form style={{marginTop: '3rem'}} onSubmit={this.handleSubmit.bind(this)}>
+                    <FormGroup style={{marginTop: '3rem'}}controlId="formBasicEmail">
+                        <Input style={{width: '26rem',height: '3rem', textAlign: 'center', fontSize: '1.4rem'}}
                             type="email"
                             name="email"
                             value={this.state.email}
                             className="text-primary"
                             onChange={this.handleChange.bind(this, 'email')}
-                            placeholder="Enter email"
+                            placeholder={this.props.match.params.lang === "LT" ? "Jūsų el. paštas" : this.props.match.params.lang === "EN" && "Your email"}
                         />
                     </FormGroup>
 
 
-                    <FormGroup controlId="formBasicMessage">
-                        <Label className="text-muted">Message</Label>
-                        <Input
+                    <FormGroup style={{marginTop: '3rem'}} controlId="formBasicMessage">
+                        {/* <Label className="text-muted">{this.props.match.params.lang === "LT" ? "Žinutė" : this.props.match.params.lang === "EN" && "Message"}<br/></Label> */}
+                        <Input style={{width: '26rem',height: '9rem', textAlign: 'left', fontSize: '1.4rem'}}
                             type="textarea"
                             name="message"
                             className="text-primary"
@@ -67,11 +68,12 @@ class Inquire extends Component {
                         />
                     </FormGroup>
 
-                    <Button variant="primary" type="submit">
-                        Submit
+                    <Button style={{width: '7rem', fontSize: '1.2rem',height: '2.2rem'}}variant="primary" type="submit">
+                    {this.props.match.params.lang === "LT" ? "Siųsti" : this.props.match.params.lang === "EN" && "Send"}
               </Button>
                 </Form>
-            </div>
+                </div >
+            </div >
         )
     }
 }
