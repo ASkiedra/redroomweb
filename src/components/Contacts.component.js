486bcae531d707fdf842fb91e1415ae26bedb2eb
Jonas <jonwow@pm.me>
2021-02-03 11:55:13 +0200

    demo email

--- src/components/Contacts.component.js
+++ src/components/Contacts.component.js
@@ -1,4 +1,82 @@
-import React from "react";
+import React, { Component } from 'react';
+
+// import * as emailjs from 'emailjs-com';
+// import { Button, FormFeedback, Form, FormGroup, Label, Input } from 'reactstrap';
+
+
+// class ContactForm extends Component {
+//     state = {
+//         email: '',
+//         message: '',
+//     }
+
+//     handleSubmit(e) {
+//         e.preventDefault();
+
+//         let templateParams = {
+//             from_name: this.state.email,
+//             to_name: 'jonas.girdzijauskas@gmail.com',
+//             message: this.state.message,
+//         }
+
+//         emailjs.send(
+//             'service_ndv2e3o',
+//             'template_f4hsl36',
+//             templateParams,
+//             'user_6JdN9Zns1wIN50N8dUi13'
+//         )
+//         this.resetForm()
+//     }
+
+//     resetForm() {
+//         this.setState({
+//             email: '',
+//             message: '',
+//         })
+//     }
+
+//     handleChange = (param, e) => {
+//         this.setState({ [param]: e.target.value })
+//     }
+
+//     render() {
+//         return (
+//             <>
+//                 <h1 className="p-heading1">Get in Touch</h1>
+//                 <Form onSubmit={this.handleSubmit.bind(this)}>
+//                     <FormGroup controlId="formBasicEmail">
+//                         <Label className="text-muted">Email address</Label>
+//                         <Input
+//                             type="email"
+//                             name="email"
+//                             value={this.state.email}
+//                             className="text-primary"
+//                             onChange={this.handleChange.bind(this, 'email')}
+//                             placeholder="Enter email"
+//                         />
+//                     </FormGroup>
+
+
+//                     <FormGroup controlId="formBasicMessage">
+//                         <Label className="text-muted">Message</Label>
+//                         <Input
+//                             type="textarea"
+//                             name="message"
+//                             className="text-primary"
+//                             value={this.state.message}
+//                             onChange={this.handleChange.bind(this, 'message')}
+//                         />
+//                     </FormGroup>
+
+//                     <Button variant="primary" type="submit">
+//                         Submit
+//               </Button>
+//                 </Form>
+//                 <Contacts/>
+//             </>
+//         )
+//     }
+// }
 
 
 const Contacts = (props) => {
@@ -20,10 +98,10 @@ const Contacts = (props) => {
                     <p>
                         {
                             props.match.params.lang === "LT" ? "SUSISIEKIME:" :
-                            props.match.params.lang === "EN" && "GET IN TOUCH:"
+                                props.match.params.lang === "EN" && "GET IN TOUCH:"
                         }
 
-                        
+
                     </p>
                     <p>+370 6 431 0657</p>
                     <p>INFO@RED-ROOM.LT</p>
@@ -33,4 +111,5 @@ const Contacts = (props) => {
     );
 }
 
-export default Contacts;
+
+export default Contacts
