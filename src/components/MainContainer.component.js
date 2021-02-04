c0d10c333aed737cd222e3e76d0e210b3b9b8dd8
Jonas <jonwow@pm.me>
2021-02-04 15:05:04 +0200

    responsivenes on the main page #2

--- src/components/MainContainer.component.js
+++ src/components/MainContainer.component.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const MainContainer = () => {
     return (
         // marginTop -2rem because the designer asked for a transparent breadcrumbs component on the main page
-        <div id="main-page-cont0" style={{ background: 'url(/images/PIANCA_226-227_Otto-Platea-Icaro-Brema_Inari_Orchestra_Venere-0d7df3b2-6180-11eb-85aa-02a8dc75caa2.jpg)', backgroundSize: 'cover', marginTop: '-5rem', paddingBottom: '12rem' }}>
+        <div id="main-page-cont0" style={{ background: 'url(/images/PIANCA_226-227_Otto-Platea-Icaro-Brema_Inari_Orchestra_Venere-0d7df3b2-6180-11eb-85aa-02a8dc75caa2.jpg)', backgroundSize: 'cover', marginTop: '-5rem', paddingBottom: '16rem' }}>
             <div></div>
 
             <div id="main-page-cont">
