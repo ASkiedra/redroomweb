4c166a6056ce648759420927be822e1301006bc1
Jonas <jonwow@pm.me>
2021-02-04 22:16:03 +0200

    interior onclick effect

--- src/components/Interior.component.js
+++ src/components/Interior.component.js
@@ -7,19 +7,29 @@ function importAll(r) {
 const images = importAll(require.context('../../public/images/interior', false, /\.(jpg|bmp|ico|png|jpe?g|svg)$/));
 
 const enlargeImage = (imgSrc) => {
-        console.log(imgSrc)
-        if (document.getElementById(imgSrc) !== null && document.getElementById(imgSrc) !== undefined)
-                document.getElementById(imgSrc).classList.toggle("enlarged");
+        if ( document.getElementById("background-container") !== undefined)
+        {
+                console.log(document.getElementById("background-container"))
+                document.getElementById("background-container").classList.toggle("display-none");
+                document.getElementById("enlarged-img").src = imgSrc;
+
+        }
 }
 
 const Interior = () => {
         return (
                 < div style={{ minHeight: 'inherit' }}>
-                        <div id="interior-container" style={{paddingBottom: '12rem'}}>
+                        <div onClick={() => document.getElementById("background-container").classList.toggle("display-none")} class="display-none" id="background-container">
+                                <div id="background-container-2">
+                                <img id="enlarged-img" src="https://i.ytimg.com/vi/MPV2METPeJU/maxresdefault.jpg"/>
+                                </div>
+
+                        </div>
+                        <div id="interior-container" style={{ paddingBottom: '12rem' }}>
                                 {
                                         images.map(element => {
                                                 return (
-                                                        <img id={element.default}  src={element.default} />
+                                                        <img onClick={(e) => enlargeImage(e.target.src)} src={element.default} />
                                                 )
                                         })
                                 }
