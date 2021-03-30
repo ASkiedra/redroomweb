e2cfd4256926f5549871878abd26137488b28117
Jonas <jonwow@pm.me>
2021-03-30 16:28:37 +0300

    auto generated text when you go to inquiries if you click on 'inquire' on a product page

--- src/components/StickyFooter.component.js
+++ src/components/StickyFooter.component.js
@@ -1,15 +1,26 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 
 const Footer = (props) => {
+    var productName;
+    const url = useLocation().pathname;
+
+    // if there are at least 6 '/' characters, it means the user is on a product page
+    if (url.split('/')[2] === 'products' && url.split('/').length >=6)
+        productName = url.split('/')[5] + ' ' + url.split('/')[6]+'. ';
 
     return (
 
         <div id="stickyFooter">
             <div id="first-footer">
                 <ul>
-                    <Link className="footer-link" to={'/' + props.language + '/inquire'}>
+                    <Link className="footer-link" to={
+                        {
+                            pathname: '/' + props.language + '/inquire',
+                            productName: productName
+                        }
+                    }>
                         <li>
                             {
                                 props.language === "LT" ? "Siųsti užklausą"
