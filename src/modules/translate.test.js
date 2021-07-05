43494b5b7e38e5f5ca0af971b62803fc2f2e5d24
Augustas Skiedra <sk.augustas@gmail.com>
2021-07-06 00:48:12 +0300

    translateMainItems tests.

--- src/modules/translate.test.js
+++ src/modules/translate.test.js
@@ -1,4 +1,5 @@
 import translateMainCats from './translate';
+import { translateMainItems } from './translate';
 
 test('translateMainCats translates properly', () => {
     expect(translateMainCats("DAY SYSTEMS")).toBe("DIENOS SISTEMOS");
@@ -13,5 +14,20 @@ test('translateMainCats translates properly', () => {
     expect(translateMainCats("OUTDOOR_FURNITURE_AND_ACCESSORIES")).not.toBe("LAUKO BALDAI IR AKSESUARAI");
 
     const junkValues = [null, undefined, NaN, 123, -123, 0, '', ' ', "", " ", "AAA", "zzz"];
+    junkValues.forEach(val => expect(translateMainCats(val)).toBe(null))
+})
+
+test('translateMainItems translates properly', () => {
+    expect(translateMainItems("CONTACTS")).toBe("KONTAKTAI");
+    expect(translateMainItems("contacts")).toBe("KONTAKTAI")
+    expect(translateMainItems("MANUFACTURERS")).toBe("GAMINTOJAI");
+    expect(translateMainItems("INTERIOR")).toBe("INTERJERAS");
+    expect(translateMainItems("PRODUCTS")).toBe("PRODUKTAI");
+    expect(translateMainItems("FURNITURE")).toBe("BALDAI");
+    expect(translateMainItems("PRIVACY POLICY")).toBe("PRIVATUMO POLITIKA");
+    expect(translateMainItems("INQUIRE")).toBe("SIŲSTI UŽKLAUSĄ");
+    expect(translateMainItems("DELIVERY")).toBe("PRISTATYMO SĄLYGOS IR TERMINAI");
+
+    const junkValues = [null, undefined, NaN, 123, '', "zzz", {}, console.log(), []];
     junkValues.forEach(val => expect(translateMainCats(val)).toBe(null))
 })
